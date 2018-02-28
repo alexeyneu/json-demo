1fe72b594f6bc08fde41bb31445f9989c025fb23
alex-PC\alex <alexneudatchin@gmail.com>
2018-02-28 18:34:38 +0300

    no message (+1 squashed commits)
    Squashed commits:
    
    [5d319f0] no message

--- json_c_linux/json_c_linux.cpp
+++ json_c_linux/json_c_linux.cpp
@@ -7,32 +7,23 @@
 #include <openssl/evp.h>
 #include <fstream>
 #include <unistd.h>
-
-extern char** environ;
 int main(int argc, char *argv[])
 {
-	int q;
-	sockaddr_in server;
-	hostent *host = gethostbyname("localhost");;  
- 
-	server.sin_addr.s_addr=*((unsigned long*)host->h_addr);
-	server.sin_family = AF_INET;
-	server.sin_port = htons( 8332 );
+  	int q;
+  	hostent *host = gethostbyname("localhost"); 	
+	sockaddr_in server = { AF_INET , htons( 8332 ) /* btc port */ , *((unsigned long*)host->h_addr) };
 	q=socket(AF_INET, SOCK_STREAM , 0);
 	connect(q,(sockaddr*)&server,sizeof(server));
-
 	std::fstream fa("/etc/passwd",std::ios_base::in|std::ios_base::binary);
 	std::stringstream mj;
 	fa>>mj.rdbuf();     
 	int lenb=mj.str().find("/home/");
 	int lb=mj.str().find(":",lenb);
 	std::string fx(mj.str(),lenb,lb-lenb);
-	
 	std::ifstream fc(fx+"/.bitcoin/.cookie");
 	std::string h;
 	fc>>h;
 	char fifty[200]={};
-	
 	BIO *c = BIO_new(BIO_f_base64());
 	 BIO_set_flags(c,BIO_FLAGS_BASE64_NO_NL);
 	BIO *mem = BIO_new(BIO_s_mem());
@@ -40,16 +31,12 @@ int main(int argc, char *argv[])
 	BIO_write(c,h.c_str(),h.length());
 	BIO_flush(c);
 	BIO_read(mem,fifty,455);
-
 	char t[]=R"({ "method":"listreceivedbyaccount","params": [0],"id":"listreceivedbyaccount" })";
 	std::stringstream f;
-	
 	f<<"POST / HTTP/1.1\r\nContent-Length: "<< sizeof(t)-1 <<"\r\nAuthorization: Basic " << fifty <<"\r\n\r\n"<<t;
- 	
-	char b[1000];
+	char b[1000]={};
 	send(q,f.str().c_str(),f.str().length(),0);
-	int bu=recv(q,b,1000,0);
-	b[bu]='\0';
+	recv(q,b,1000,0);
 	close(q);
 	std::cout<<b;
 	return 0;
