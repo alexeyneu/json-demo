08a48288a2c5ea62958d16654f1c0d3b7f77045b
alex-PC\alex <alexneudatchin@gmail.com>
2018-02-28 19:19:42 +0300

    no message

--- json_c.cpp
+++ json_c.cpp
@@ -1,28 +1,19 @@
 #include <cstdint>
 #include <iostream>
 #include <WinSock2.h>
-#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // some CString constructors will be explicit
 #include <atlenc.h>
 #include <sstream>
 #include <fstream>
-
 #pragma comment(lib, "ws2_32")
 
 int wmain(int argc, wchar_t* argv[])
 {
-	WSADATA fc;
+	WSADATA fc;56
 	WSAStartup(0x202,&fc); //sockets 2.02 ,hibyte and lobyte = 2 
-	SOCKET q;
-	sockaddr_in server;
-  //	sockaddr_in ;
-	hostent *host;  
-	host = gethostbyname("localhost");
+	hostent *host = gethostbyname("localhost");
+	sockaddr_in server = { AF_INET , htons(8556) /* btx port */ , *((unsigned long*)host->h_addr) };
 
-	server.sin_addr.s_addr=*((unsigned long*)host->h_addr);
-	server.sin_family = AF_INET;
-	server.sin_port = htons( 8556 ); // btx port
-
-	q=socket(AF_INET, SOCK_STREAM , 0);
+	SOCKET q = socket(AF_INET, SOCK_STREAM , 0);
 	connect(q,(sockaddr*)&server,sizeof(server));
 	
 	wchar_t w[140];
@@ -34,17 +25,15 @@ int wmain(int argc, wchar_t* argv[])
 	char fifty[200]={};
 	int len=Base64EncodeGetRequiredLength(h.length());
 	Base64Encode((BYTE *)h.c_str(),h.length(),fifty,&len,ATL_BASE64_FLAG_NOCRLF);
-	char t[]="{ \"method\":\"listrece\
-ivedbyaccount\",\"params\": [0],\"id\":\"listreceivedbyaccount\" }\r\n";
+	char t[]="{ \"method\":\"listreceivedbyaccount\",\"params\": [0],\"id\":\"listreceivedbyaccount\" }";
 
 	std::stringstream f;
 	
 	f<<"POST / HTTP/1.1\r\nContent-Length: "<< sizeof(t)-1 <<"\r\nAuthorization: Basic " << fifty <<"\r\n\r\n"<<t;
  
-	
 	char b[1000];
-	send(q,f.str().c_str(),f.str().length(),0);
 	
+	send(q,f.str().c_str(),f.str().length(),0);	
 	len=recv(q,b,1000,0);
 
 	b[len]='\0';
@@ -52,4 +41,3 @@ ivedbyaccount\",\"params\": [0],\"id\":\"listreceivedbyaccount\" }\r\n";
 	std::cout<<b;
 	return 0;
 }
-
