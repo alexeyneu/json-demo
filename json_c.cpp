328e46ba08d170128af48fe64ffecb5e78e596f6
Alexey <31589671+alexeyneu@users.noreply.github.com>
2018-04-27 09:50:48 +0300

    Update json_c.cpp

--- json_c.cpp
+++ json_c.cpp
@@ -13,29 +13,23 @@ int wmain(int argc, wchar_t* argv[])
 	hostent *host = gethostbyname("localhost");
 	sockaddr_in server = { AF_INET , htons(8556) /* btx port */ , *((unsigned long*)host->h_addr) };
 	SOCKET q = socket(AF_INET, SOCK_STREAM , 0);
-	/* tcp/ip connection established ,initially packets exchange */
 	connect(q,(sockaddr*)&server,sizeof(server));
-	/* login and password  */
 	wchar_t w[140];
 	ExpandEnvironmentStrings(L"%APPDATA%\\Bitcore\\.cookie",w,140);
 	std::ifstream fa(w);  
 	std::string h;
 	fa>>h;
-	/*  continued  */
 	char fifty[200]={};
 	int len=Base64EncodeGetRequiredLength(h.length());
 	Base64Encode((BYTE *)h.c_str(),h.length(),fifty,&len,ATL_BASE64_FLAG_NOCRLF);
 	char t[]="{ \"method\":\"listreceivedbyaccount\",\"params\": [0],\"id\":\"listreceivedbyaccount\" }";
 	/* built-in json server dealt with  */
 	std::stringstream f;
-	
 	f<<"POST / HTTP/1.1\r\nContent-Length: "<< sizeof(t)-1 <<"\r\nAuthorization: Basic " << fifty <<"\r\n\r\n"<<t;
- 
 	char b[1000];
 	/*  wallet   */	
 	send(q,f.str().c_str(),f.str().length(),0);	
 	len=recv(q,b,1000,0);
-
 	b[len]='\0';
 	closesocket(q);
 	std::cout<<b;
