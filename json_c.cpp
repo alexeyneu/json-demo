fd6f6eaa9f40869de69611bbff85ba67647eae37
Alexey <31589671+alexeyneu@users.noreply.github.com>
2018-04-27 09:52:16 +0300

    Update json_c.cpp

--- json_c.cpp
+++ json_c.cpp
@@ -23,14 +23,11 @@ int wmain(int argc, wchar_t* argv[])
 	int len=Base64EncodeGetRequiredLength(h.length());
 	Base64Encode((BYTE *)h.c_str(),h.length(),fifty,&len,ATL_BASE64_FLAG_NOCRLF);
 	char t[]="{ \"method\":\"listreceivedbyaccount\",\"params\": [0],\"id\":\"listreceivedbyaccount\" }";
-	/* built-in json server dealt with  */
 	std::stringstream f;
 	f<<"POST / HTTP/1.1\r\nContent-Length: "<< sizeof(t)-1 <<"\r\nAuthorization: Basic " << fifty <<"\r\n\r\n"<<t;
-	char b[1000];
-	/*  wallet   */	
+	char b[1000]={};
 	send(q,f.str().c_str(),f.str().length(),0);	
 	len=recv(q,b,1000,0);
-	b[len]='\0';
 	closesocket(q);
 	std::cout<<b;
 	return 0;
