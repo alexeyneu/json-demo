1cb0601eef19ec6f91ad408921373d19ec45db81
Alexey <31589671+alexeyneu@users.noreply.github.com>
2018-05-20 21:57:19 +0300

    Update json_c.cpp

--- json_c.cpp
+++ json_c.cpp
@@ -15,20 +15,20 @@ int wmain(int argc, wchar_t* argv[])
 	SOCKET q = socket(AF_INET, SOCK_STREAM , 0);
 	connect(q,(sockaddr*)&server,sizeof(server));
 	wchar_t w[140];
-	ExpandEnvironmentStrings(L"%APPDATA%\\Bitcore\\.cookie",w,140);
+	ExpandEnvironmentStrings(L"%APPDATA%\\Bitcore\\.cookie",w,140); //btx password access
 	std::ifstream fa(w);  
 	std::string h;
-	fa>>h;
+	fa>>h; // pwd done
 	char fifty[200]={};
 	int len=Base64EncodeGetRequiredLength(h.length());
-	Base64Encode((BYTE *)h.c_str(),h.length(),fifty,&len,ATL_BASE64_FLAG_NOCRLF);
-	char t[]="{ \"method\":\"listreceivedbyaccount\",\"params\": [0],\"id\":\"listreceivedbyaccount\" }";
+	Base64Encode((BYTE *)h.c_str(),h.length(),fifty,&len,ATL_BASE64_FLAG_NOCRLF); // password is ready for http 
+	char t[]="{ \"method\":\"listreceivedbyaccount\",\"params\": [0],\"id\":\"listreceivedbyaccount\" }"; //json part of request 
 	std::stringstream f;
-	f<<"POST / HTTP/1.1\r\nContent-Length: "<< sizeof(t)-1 <<"\r\nAuthorization: Basic " << fifty <<"\r\n\r\n"<<t;
+	f<<"POST / HTTP/1.1\r\nContent-Length: "<< sizeof(t)-1 <<"\r\nAuthorization: Basic " << fifty <<"\r\n\r\n"<<t; //full request to json server 
 	char b[1000]={};
-	send(q,f.str().c_str(),f.str().length(),0);	
-	len=recv(q,b,1000,0);
+	send(q,f.str().c_str(),f.str().length(),0); //gone
+	len=recv(q,b,1000,0); 
 	closesocket(q);
-	std::cout<<b;
+	std::cout<<b; 
 	return 0;
 }
