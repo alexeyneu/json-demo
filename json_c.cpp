3489c65a1eb89b6d8414e7f2b01a6674bb4fa32c
alex-PC\alex <alexneudatchin@gmail.com>
2018-06-16 18:54:40 +0400

    no message

--- json_c.cpp
+++ json_c.cpp
@@ -4,7 +4,6 @@
 #include <sstream>
 #include <fstream>
 #pragma comment(lib, "ws2_32")
-
 int wmain(int argc, wchar_t* argv[])
 {
 	WSADATA fc;
@@ -18,15 +17,14 @@ int wmain(int argc, wchar_t* argv[])
 	std::ifstream fa(w);  
 	std::string h;
 	fa>>h; // pwd done
-	char fifty[200]={};
+	char fifty[200]={}, b[1000]={};
 	int len=Base64EncodeGetRequiredLength(h.length());
-	Base64Encode((BYTE *)h.c_str(),h.length(),fifty,&len,ATL_BASE64_FLAG_NOCRLF); // password is ready for http 
+	Base64Encode((BYTE *)h.c_str(), h.length(), fifty, &len, ATL_BASE64_FLAG_NOCRLF); // password is ready for http 
 	char t[]="{ \"method\":\"listreceivedbyaccount\",\"params\": [0],\"id\":\"listreceivedbyaccount\" }"; //json part of request 
 	std::stringstream f;
 	f<<"POST / HTTP/1.1\r\nContent-Length: "<< sizeof(t)-1 <<"\r\nAuthorization: Basic " << fifty <<"\r\n\r\n"<<t; //full request to json server 
-	char b[1000]={};
-	send(q,f.str().c_str(),f.str().length(),0); //gone
-	len=recv(q,b,1000,0); 
+	send(q,f.str().c_str(), f.str().length() ,0); //gone
+	recv(q,b,1000,0); 
 	closesocket(q);
 	std::cout<<b; 
 	return 0;
