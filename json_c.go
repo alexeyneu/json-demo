eea27ff67dbe64cfd7a9118ff32d1ae34c892f6f
alex-PC\alex <alexneudatchin@gmail.com>
2018-06-18 15:22:47 +0400

    no message

--- json_c.go
+++ json_c.go
@@ -8,17 +8,16 @@ import (
 )
 //Bitcore
 func main() {
-	s:=os.ExpandEnv("${APPDATA}\\Bitcore\\.cookie")
+	s:=os.ExpandEnv("${APPDATA}\\Bitcore\\.cookie")  // linux style but it's windows
 	data, err := ioutil.ReadFile(s)
 	if err!=nil {
 		fmt.Fprintf(os.Stderr, " %v\n", err)
 		os.Exit(1)
 	}
-	t:=string(data[11:])     // len("__cookie__:")
+	t:=string(data[11:])     // "__cookie__:"
 	client := &http.Client{}
-	endPoint := "http://localhost:8556"
 	z:=`{ "method":"listreceivedbyaccount","params": [0],"id":"listreceivedbyaccount" }`
-	req, _ := http.NewRequest("POST", endPoint, bytes.NewBuffer([]byte(z))) // "Content-Length: ...\r\n" is also needed in this request , but it dealt with by golang. value is len(z)
+	req, _ := http.NewRequest("POST","http://localhost:8556" , bytes.NewBuffer([]byte(z))) // "Content-Length: ...\r\n" is also needed in this request , but it dealt with by golang. value is len(z)
 	req.SetBasicAuth("__cookie__",t) // what's  added exactly is "Authorization: Basic base64_encoded_name+:+pwd\r\n"
 	response, _ := client.Do(req)
 	bt , _:=ioutil.ReadAll(response.Body)
